3cecd719bdc1f245118c7b0e0e4dce079e477ddf
AITown <l178802263@live.com>
2018-06-28 15:03:05 +0800

    add ignore

--- etcdset/main.go
+++ etcdset/main.go
@@ -18,7 +18,7 @@ type collectionInfo struct {
 
 func main() {
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379,192.168.5.31:2379"},
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
